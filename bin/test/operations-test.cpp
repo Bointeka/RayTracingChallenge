fe1143629850174f46b79eb0ccbc9e8425584c1c
Bointeka <jkaoga@gmail.com>
2021-08-04 15:30:14 -0400

    Created Operations testing as well as the operation files

---
+++ bin/test/operations-test.cpp
@@ -0,0 +1,91 @@
+#include "../features/header files/Tuples.h"
+#include "../features/header files/Operations.h"
+#include <cassert>
+#include <iostream>
+
+using namespace Features;
+using namespace std;
+
+
+void TEST_TUPLE_ADD() {
+    cout << "TESTING TUPLE ADD TEST" << endl;    
+    Vector vec1 = Vector();
+    Vector vec2 = Vector();
+    Point point1 = Point();
+    assert(tuple_sum(vec1, vec2) == Vector(2,2,2));
+    assert(tuple_sum(vec1, point1) == Point(1,1,1));
+    assert(tuple_sum(point1, point1) == NULL);
+    cout << "TEST PASSED" << endl;
+}
+
+void TEST_TUPLE_SUBTRACT() {
+    cout << "TESTING TUPLE SUBTRACT TEST" << endl;    
+    Vector vec1 = Vector();
+    Vector vec2 = Vector();
+    Point point1 = Point();
+    Point point2 = Point(1,1,1);
+    assert(tuple_sub(vec1, vec2) == Vector(0,0,0));
+    assert(tuple_sub(point1, vec1) == Point(0,0,0));
+    assert(tuple_sub(point1, point2) == Vector(-1,-1,-1));
+    assert(tuple_sub(vec1, point1) == NULL);
+    cout << "TEST PASSED" << endl;   
+}
+
+void TEST_TUPLE_NEGATE() {
+    cout << "TESTING TUPLE NEGATE" << endl;
+    Vector vec1 = Vector();
+    assert(negate(vec1) == Vector(-1,-1,-1));
+    cout << "TEST PASSED" << endl;
+}
+
+void TEST_TUPLE_SCALAR() {
+    cout << "TESTING TUPLE SCALAR" << endl;
+    Vector vec1 = Vector();
+    assert(vec1.scalar(2) == Vector(2,2,2));
+    assert(vec1.scalar(0.5) == Vector(0.5, 0.5, 0.5));
+    cout << "TEST PASSED" << endl;
+}
+
+void TEST_TUPLE_MAGNITUDE() {
+    cout << "TESTING TUPLE MAGNITUDE" << endl;
+    Vector vec1 = Vector(1,2,1);
+    assert(magnitude(vec1) == 3);
+    cout << "TEST PASSED" << endl;
+}
+
+void TEST_TUPLE_NORMALIZATION() {
+    cout << "TESTING TUPLE NORMALIZATION" << endl;
+    Vector vec1 = Vector(1,2,1);
+    float magnitude = magnitude(vec1);
+    assert(normalize(vec1) == Vector(1 / magnitude, 2 / magnitude, 3 / magnitude));
+    cout << "TEST PASSED" << endl;
+}
+
+void TEST_TUPLE_DOT() {
+    cout << "TESTING TUPLE DOT" << endl;
+    Vector vec1 = Vector(1,2,3);
+    Vector vec2 = Vector(2,3,4);
+    assert(dot(vec1,vec2) == 20);
+    cout << "TEST PASSED" << endl;
+}
+
+void TEST_TUPLE_CROSS() {
+    cout << "TESTING TUPLE CROSS" << endl;
+    Vector vec1 = Vector(1,2,3);
+    Vector vec2 = Vector(2,3,4);
+    assert(cross(vec1, vec2) == Vector(-1, 2, -1));
+    assert(cross(vec2, vec1) == Vector(1, -2, 1));
+    cout << "TEST PASSED" << endl;
+}
+
+int main() {
+    TEST_TUPLE_ADD();
+    TEST_TUPLE_SUBTRACT();
+    TEST_TUPLE_NEGATE();
+    TEST_TUPLE_MAGNITUDE();
+    TEST_TUPLE_SCALAR();
+    TEST_TUPLE_NORMALIZATION();
+    TEST_TUPLE_DOT();
+    TEST_TUPLE_CROSS();
+    return 1;
+}
