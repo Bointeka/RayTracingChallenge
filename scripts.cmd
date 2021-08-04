19b97c111947e9a06dfa23c507a174e7474bdfeb
Bointeka <jkaoga@gmail.com>
2021-08-04 15:35:14 -0400

    Added next test suite to deployment script

--- scripts.cmd
+++ scripts.cmd
@@ -1,17 +1,22 @@
 @echo off
+REM FIND Way to check of object file already exists
 C:
 cd \Users\jkaog\Desktop\Coding\RayTracing Challenge\bin\obj
 
 REM compling source files
-gcc -g -c  "..\features\source files\Tuple.cpp"
+g++ -g -c  "..\features\source files\Tuple.cpp"
+g++ -g -c  "..\features\source files\Operations.cpp"
+
 
 REM compiling test files
 g++ -g -c ../test/tuple-test.cpp
+g++ -g -c ../test/operations-test.cpp
 
 
 REM linking object files and test files
-g++ -o ../../tuple-test tuple-test.o Tuple.o
+g++ -o ../../test_exe/tuple-test tuple-test.o Tuple.o
+g++ -o ../../test_exe/operation-test operations-test.o Opertaions.o
 
 REM Run tests
 @echo on
-START ../../tuple-test.exe
+START ../../test_exe/tuple-test.exe
