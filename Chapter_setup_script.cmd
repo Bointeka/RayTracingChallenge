95999235e4ed7b3cdaf6d1c359a3e4c9963ae380
Bointeka <jkaoga@gmail.com>
2021-08-16 16:13:16 -0400

    Added checks to prevent unnecessary compiling in test_setup_scripts.cmd. Added comments to Chapter_setup_script.cmd for future reference

--- Chapter_setup_script.cmd
+++ Chapter_setup_script.cmd
@@ -1,13 +1,23 @@
 @echo off
+REM Change directories to the putting it together chapter
 C:
-cd \Users\jkaog\Desktop\Coding\RayTracing Challenge\putting_it_together
+cd \Users\jkaog\Desktop\Coding\RayTracing Challenge\putting_it_together\Chapter 1
 
-g++ -g -c ".\Chapter 1\Projectile.cpp"
-g++ -g -c ".\Chapter 1\main.cpp"
+REM Make a bin directory if it does not exist
+if NOT EXIST bin\NUL mkdir bin
 
+REM Move to the bin directory
+cd bin
+
+REM Compile the relevant files
+g++ -g -c "..\Projectile.cpp"
+g++ -g -c "..\main.cpp"
+
+REM Change to the directory of the core files' object files
 C:
 cd \Users\jkaog\Desktop\Coding\RayTracing Challenge\bin\obj
 
+REM Check if all the relevant object files exist
 if NOT EXIST Tuple.o (
     C:
     cd \Users\jkaog\Desktop\Coding\RayTracing Challenge   
@@ -19,7 +29,9 @@ if NOT EXIST Operations.o (
     CALL test_setup_scripts.cmd
 )
 
+REM Revert back to the Chapter's bin file
 C:
-cd \Users\jkaog\Desktop\Coding\RayTracing Challenge\putting_it_together
+cd \Users\jkaog\Desktop\Coding\RayTracing Challenge\putting_it_together\Chapter 1\bin
 
-g++ -o main main.o Projectile.o ..\bin\obj\Operations.o ..\bin\obj\Tuple.o
+REM Link 
+g++ -o main main.o Projectile.o ..\..\..\bin\obj\Operations.o ..\..\..\bin\obj\Tuple.o
--- test_setup_scripts.cmd
+++ test_setup_scripts.cmd
@@ -1,16 +1,20 @@
 @echo off
+REM TODO prevent script from compiling unnecessarily
 REM FIND Way to check of object file already exists
 C:
-cd \Users\jkaog\Desktop\Coding\RayTracing Challenge\bin\obj
+cd \Users\jkaog\Desktop\Coding\RayTracing Challenge\bin
+
+if NOT EXIST obj\NUL mkdir obj
+cd obj
 
 REM compling source files
-g++ -g -c  "..\features\source files\Tuple.cpp"
-g++ -g -c  "..\features\source files\Operations.cpp"
+if NOT EXIST Tuple.o g++ -g -c  "..\features\source files\Tuple.cpp"
+if NOT EXIST Operations.o g++ -g -c  "..\features\source files\Operations.cpp"
 
 
 REM compiling test files
-g++ -g -c ../test/tuple-test.cpp
-g++ -g -c ../test/operations-test.cpp
+if NOT EXIST tuple-test.o g++ -g -c ../test/tuple-test.cpp
+if NOT EXIST operations-test.o g++ -g -c ../test/operations-test.cpp
 
 
 REM linking object files and test files
